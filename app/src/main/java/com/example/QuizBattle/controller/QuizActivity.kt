78e5af8733cb43360eb931ec6aa6a9e428297377
Nima Hakimi <hakimi.nima1@gmail.com>
2023-04-06 19:21:44 +0200

    Create new layout for the quiz. Changge the activity to follow that layout

--- app/src/main/java/com/example/QuizBattle/controller/QuizActivity.kt
+++ app/src/main/java/com/example/QuizBattle/controller/QuizActivity.kt
@@ -1,31 +1,43 @@
 package com.example.QuizBattle.controller
 
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.TextView
 import com.example.QuizBattle.R
 
 class QuizActivity: GameState  {
 
-
-    /*override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.question)
-        val questionText: TextView=findViewById(R.id.que)
-        val q : String ="A question here ...?"
-        questionText.text = q
-
-        }*/
-    private lateinit var goback: Button
     override fun handleView(activity: MenuActivity) {
-        activity.setContentView(R.layout.question)
-        val questionText:TextView=activity.findViewById(R.id.que)
-        val q : String ="A question here ...?"
-        questionText.text = q
-        goback=activity.findViewById(R.id.GoBackQuiz)
-        goback.setOnClickListener {
-            // Transition to the next state
+        activity.setContentView(R.layout.quiz)
+
+        val questionText: TextView = activity.findViewById(R.id.question_text)
+        val question: String = "A question here ...?"
+        questionText.text = question
+
+        // RadioButtons
+        val answerA: RadioButton = activity.findViewById(R.id.answer_A)
+        val answerB: RadioButton = activity.findViewById(R.id.answer_B)
+        val answerC: RadioButton = activity.findViewById(R.id.answer_C)
+        val answerD: RadioButton = activity.findViewById(R.id.answer_D)
+
+        val radioButtons = listOf(answerA, answerB, answerC, answerD)
+
+        // Set up RadioButton click listeners
+        for (radioButton in radioButtons) {
+            radioButton.setOnClickListener {
+                for (otherRadioButton in radioButtons) {
+                    if (otherRadioButton != it) {
+                        otherRadioButton.isChecked = false
+                    }
+                }
+            }
+        }
+
+        // Submit button
+        val submitButton: Button = activity.findViewById(R.id.submit_button)
+        submitButton.setOnClickListener {
+            // Transition to the next state (HomeMenuActivity in this case)
             activity.setState(HomeMenuActivity())
         }
     }
-
 }
