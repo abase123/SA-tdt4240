d278df05fd74f9659295e97967954f56db7eb401
unknown <abasinessa@gmail.com>
2023-04-14 17:53:46 +0200

    small start on noti

--- app/src/main/java/com/example/QuizBattle/controller/NotificationService.kt
+++ app/src/main/java/com/example/QuizBattle/controller/NotificationService.kt
@@ -1,7 +1,18 @@
 package com.example.QuizBattle.controller
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlin.random.Random
 
 class NotificationService:FirebaseMessagingService() {
 
@@ -10,6 +21,39 @@ class NotificationService:FirebaseMessagingService() {
     }
 
     private fun sendNotification(title:String?,body:String?){
+        val channelId = "your_channel_id"
+        val notificationId = Random.nextInt()
+        val intent = Intent(this, GameController::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.your_notification_icon)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+    }
+
+
+    @SuppressLint("ServiceCast")
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "your_channel_id"
+            val channelName = "Your Channel Name"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = "Your Channel Description"
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
