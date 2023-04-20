b2f53dd935dd7e26c6e4fd00a3f03825aa97b516
abase123 <abasinessa@gmail.com>
2023-04-20 11:45:57 +0200

    lessgo

---
+++ app/src/main/java/com/example/QuizBattle/model/FirestoreRepoes/FirestoreRepoOnlineQuiz.kt
@@ -0,0 +1,22 @@
+package com.example.QuizBattle.model.FirestoreRepoes
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+class FirestoreRepoOnlineQuiz {
+    private val database = FirebaseFirestore.getInstance()
+    private val quizRef = database.collection("active_games")
+
+
+    private fun findGame(){
+
+
+    }
+
+
+    private fun createGame(){
+
+
+    }
+
+
+}
