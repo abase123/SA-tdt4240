fdc4367515a5a571c56a724e11e7af1f18b85b9f
Pravin Thevakan <pravin20111@hotmail.com>
2023-04-20 17:54:06 +0200

    choose theme

--- app/src/main/java/com/example/QuizBattle/framgmentsControllers/ChooseThemeView.kt
+++ app/src/main/java/com/example/QuizBattle/framgmentsControllers/ChooseThemeView.kt
@@ -1,20 +1,47 @@
 package com.example.QuizBattle.framgmentsControllers
 
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.example.QuizBattle.R
 import com.example.QuizBattle.controller.GameController
 import com.example.QuizBattle.controller.ViewChangeListener
+import com.example.QuizBattle.controller.gameStates.PlayDailyQuizState.PlayDailyQuizViewModel
 
 class ChooseThemeView : Fragment(){
-    private lateinit var descriptionTextView: TextView
-    private lateinit var startQuizButton: Button
-    private lateinit var goBackButton: Button
-    private lateinit var loadingProgressBar: ProgressBar
+    private lateinit var choseText: TextView
+    private val themes = mutableListOf<RadioButton>()
+    private lateinit var chosenTheme: RadioButton
     private var viewChangeListener: ViewChangeListener? = null
-    private lateinit var themeImage: ImageView
-    private lateinit var diffTextView: TextView
     private lateinit var gameController: GameController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_chose_theme, container, false) as ViewGroup
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is ViewChangeListener) {
+            viewChangeListener = context
+        } else {
+            throw RuntimeException("$context must implement UserInputListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        viewChangeListener = null
+    }
+
+    private fun showChosentTheme() {
+        chosenTheme.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.chosen_theme))
+    }
 }
